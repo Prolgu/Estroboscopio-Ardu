7f6e410ffddd0888a7348ebd24a48bf99c00addb
Prolgu <mjolner_@hotmail.com>
2020-03-22 20:47:34 -0300

    Editado el archivo .ino

--- Estroboscopio-Ardu/Estroboscopio-Ardu.ino
+++ Estroboscopio-Ardu/Estroboscopio-Ardu.ino
@@ -1,7 +1,10 @@
+//De base estoy utilizando un arduino mega 2560 la idea general 
+//seria utilizar un arduino nano, con eso se podria crear un dispositivo
+//como los que se pueden encontrar en el mercado(tipo pistola de lectura)
 //En el A0 conectamos el el cursor del potenciometro (pata central)
 //las otras dos patas van conectadas a GND y +5V del Arduino
 //con map() en el A0 usamos el DAC propio de arduino para
-//convertir una tension analogica en datos digitales 
+//convertir una tension analogica en datos digitales
 //de esta forma podemos operar pasando el dato leido a una variable
 //que va a ser la que genere el delay entre un estado y alto
 //del led (ledPin=13)
@@ -10,7 +13,7 @@
 //solo imprima cuando se lo ordene por medio de un boton (botonPin)
 //al conectarse el terminal serial mando la cadena Estroboscopio-Ardu
 //de esta forma me aseguro de la correcta conexion con el pc
-//estoy ppensando implementar el uso de una sola libreria para imprimir en 
+//estoy ppensando implementar el uso de una sola libreria para imprimir en
 //un lcd, por medio del protocolo I2C.
 
 
@@ -18,7 +21,10 @@
 
 const int ledPin = 13;//constantes
 const int botonPin = 2;//constantes
-
+const int sensorPin = A0;
+int poteVuualor = 0;
+float freq = 0;
+float valorSalida = 0;
 
 void setup() {
  pinMode(ledPin, OUTPUT); //defino ledPin como salida
@@ -34,12 +40,12 @@ void setup() {
 
 void loop() {
 
-  int poteValor = analogRead(A0);
-  int delay_ms=map(poteValor,0,1023,10,150);//divido el valor del pote en  1024 partes equivalentes a 10-150ms
-  Generador(ledPin,delay_ms);//llamo a la funcion y le doy los argumentos
-  //delay(1000);
-  Serial.print(delay_ms);//imprimo en la terminal el valor en ms
-  //Serial.println(" Rpm");// para cuando haga la conversion
+  poteValor = analogRead(sensorPin);
+  valorSalida=map(poteValor,0,1023,10,150);//divido el valor del pote en  1024 partes equivalentes a 10-150ms
+  Generador(ledPin,valorSalida);//llamo a la funcion y le doy los argumentos
+  Serial.print("RPM = ");
+  freq = (1000)/(valorSalida+1);
+  Serial.println(60*freq);
 }
 
 //con esta funcion gneramos un puso cuadrado.
