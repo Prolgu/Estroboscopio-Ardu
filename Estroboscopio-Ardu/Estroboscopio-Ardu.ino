a298d2498e59d6a103243b3b99eb0883c148b7ef
Prolgu <mjolner_@hotmail.com>
2020-03-22 18:38:36 -0300

    Agregados comentarios al .ino

--- Estroboscopio-Ardu/Estroboscopio-Ardu.ino
+++ Estroboscopio-Ardu/Estroboscopio-Ardu.ino
@@ -1,3 +1,21 @@
+//En el A0 conectamos el el cursor del potenciometro (pata central)
+//las otras dos patas van conectadas a GND y +5V del Arduino
+//con map() en el A0 usamos el DAC propio de arduino para
+//convertir una tension analogica en datos digitales 
+//de esta forma podemos operar pasando el dato leido a una variable
+//que va a ser la que genere el delay entre un estado y alto
+//del led (ledPin=13)
+//el dato analogico va a ser entre 0-1023 siendo equivalente a 10-150ms
+//todavia sigo trabajando en la mejor forma de que el serial
+//solo imprima cuando se lo ordene por medio de un boton (botonPin)
+//al conectarse el terminal serial mando la cadena Estroboscopio-Ardu
+//de esta forma me aseguro de la correcta conexion con el pc
+//estoy ppensando implementar el uso de una sola libreria para imprimir en 
+//un lcd, por medio del protocolo I2C.
+
+
+
+
 const int ledPin = 13;//constantes
 const int botonPin = 2;//constantes
 
@@ -7,11 +25,11 @@ void setup() {
  pinMode(botonPin, INPUT); //defino botonPin como entrada
  Serial.begin(9600);   // inicializa el serial para mandar datos
    while (!Serial) {
-    ; 
+    ;
     }
 
   // Titulo con salto de linea
-  Serial.println("Estroboscopio");
+  Serial.println("Estroboscopio-Ardu");
 }
 
 void loop() {
@@ -33,5 +51,5 @@ void Generador(int a,int b)//defino la funcion Generador con
   delay(b);//espero b ms
   digitalWrite(a,0);//escribo un estado bajo en el pin
   delay(b);//espero b ms
- 
+
 }
